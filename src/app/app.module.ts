9296788b294e0cbe680467059c3523f6281b055d
Edgardo Luis Lugo Blanco <32710186+edgardoll4@users.noreply.github.com>
2022-10-24 16:03:41 -0400

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
+import { PaginatePipe } from './pipes/paginate.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/body/body.component.ts
+++ src/app/shared/body/body.component.ts
@@ -36,28 +36,6 @@ export class BodyComponent implements OnInit {
   //       "watsapp_id": "wamid.HBgMNTg0MTI0NzM2NjI2FQIAEhgUM0VCMDFGRDgzMjE1QkIyQzdFRUUA",
   //       "from": "584124736626",
   //       "phone_number_id": "107490002110448"
-  //   },
-  //   {
-  //       "id": "8649c8c3-14fe-4064-aa9d-be0294536ced",
-  //       "text": "Anular cita",
-  //       "name": "Lcdo Edgardo Lugo Blanco",
-  //       "type": "button",
-  //       "payload": "token_anular",
-  //       "timestamp": new Date(1664322883).toISOString(),
-  //       "watsapp_id": "wamid.HBgMNTg0MTI0NzM2NjI2FQIAEhgUM0VCMDY0ODM1QkJCRkY3NjQ4QTIA",
-  //       "from": "584124736626",
-  //       "phone_number_id": "107490002110448"
-  //   },
-  //   {
-  //       "id": "a8a88f79-9d57-474f-8da2-7be59c46a2a0",
-  //       "text": "Asistiré",
-  //       "name": "Lcdo Edgardo Lugo Blanco",
-  //       "type": "button",
-  //       "payload": "token_confirmar",
-  //       "timestamp": new Date(1664322937).toISOString(),
-  //       "watsapp_id": "wamid.HBgMNTg0MTI0NzM2NjI2FQIAEhgUM0VCMDIyOTBGMzI0QURBNEI0Q0YA",
-  //       "from": "584124736626",
-  //       "phone_number_id": "107490002110448"
   //   }
   // ];
 
@@ -65,9 +43,13 @@ export class BodyComponent implements OnInit {
     this.getMesseges();
   }
 
+  limit: number = 20;
+  pageNumber: number = 1;
+  pageOption: number[] = [10, 20, 50, 100];
+
   getMesseges() {
     console.log('getMesseges()');
-    this.apiWsMeseege.getMesseges(10, 0)
+    this.apiWsMeseege.getMesseges(this.limit, 0)
       .subscribe((resp: Messege[]) => {
         this.meseeges = resp;
       })
